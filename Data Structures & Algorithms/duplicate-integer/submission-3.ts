de5975ef0bf26a0e0a2424e3a2b0eb373d497103
Koti Reddy <98949858+Kotireddy-Kolli@users.noreply.github.com>
2026-08-04 10:21:24 +0530

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+
+    /**
+     * we can conver to set and compare the length (or)
+     * we can check each element and if it is not in a set/map we can add else return false
+     */
+    hasDuplicate(nums: number[]): boolean {
+        let uniqueElements = {};
+        for(let el of nums) {
+            if(uniqueElements.hasOwnProperty(el)) {
+                return true;
+            } else {
+                uniqueElements[el]=el;
+            }
+        }
+        return false;
+    }
+}
