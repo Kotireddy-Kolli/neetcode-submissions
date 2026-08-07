bdfcc4c96df554a06f309337c579a2be1b6b9c50
Koti Reddy <98949858+Kotireddy-Kolli@users.noreply.github.com>
2026-08-07 07:46:15 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let map = new Map();
+        for(let str of strs) {
+            const count = Array(26).fill(0);
+            for (let i = 0; i < str.length; i++) {
+                count[str.charCodeAt(i) - 97]++;
+            }
+            const key = count.toString();
+
+            if(!map.has(key)) map.set(key, []);
+            map.get(key).push(str);
+        }
+        return Array.from(map.values())
+    }
+}
